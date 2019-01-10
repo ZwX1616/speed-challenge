8f97db0a2000611688962f6e40493dd0a922ec50
Justin Glibert <justin@glibert.io>
2019-01-10 16:48:27 +0100

    wip

--- forward.sh
+++ forward.sh
@@ -1,2 +1,2 @@
-sudo ssh -i "/Users/justinglibert/.ssh/ec2-gpu-ireland.pem" -L 443:127.0.0.1:8888 -R 52698:127.0.0.1:52698 ubuntu@ec2-34-244-160-108.eu-west-1.compute.amazonaws.com
+sudo ssh -i "/Users/justinglibert/.ssh/ec2-gpu-ireland.pem" -L 443:127.0.0.1:8888 -R 52698:127.0.0.1:52698 ubuntu@ec2-54-229-200-183.eu-west-1.compute.amazonaws.com
 
